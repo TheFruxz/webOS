41d5112db58bebc2ab6a3ffe8f8849f84abfa9c4
TheFruxz <cedricspitzer@outlook.de>
2023-12-28 22:43:32 +0100

    chore: update window manager to more utilize window uuids

--- src/lib/manager/WindowManager.ts
+++ src/lib/manager/WindowManager.ts
@@ -15,8 +15,11 @@ const WindowManager = {
     getHiddenWindows(): Window[] {
         return openWindows.filter((window) => hiddenWindowIDs.indexOf(window.uuid) !== -1);
     },
-    getWindow(id: number): Window {
-        return openWindows[id];
+    getWindowByOrder(index: number): Window {
+        return openWindows[index];
+    },
+    getWindow(windowUUID: string): Window | undefined {
+        return openWindows.find((window) => window.uuid === windowUUID);
     },
     forEach(callback: (window: Window, id: number) => void): void {
         openWindows.forEach(callback);
@@ -25,28 +28,48 @@ const WindowManager = {
         openWindows.push(window);
         windows.set(openWindows);
     },
-    close(window: Window): void {
-        openWindows = openWindows.filter((w) => w.uuid !== window.uuid);
+    close(windowUUID: string): void {
+        openWindows = openWindows.filter((w) => w.uuid !== windowUUID);
         windows.set(openWindows);
     },
-    hide(window: Window): void {
-        hiddenWindowIDs.push(window.uuid);
+    hide(windowUUID: string): void {
+        hiddenWindowIDs.push(windowUUID);
     },
-    show(window: Window): void {
-        hiddenWindowIDs = hiddenWindowIDs.filter((id) => id !== window.uuid);
+    show(windowUUID: string): void {
+        hiddenWindowIDs = hiddenWindowIDs.filter((id) => id !== windowUUID);
     },
-    isHidden(window: Window): boolean {
-        return hiddenWindowIDs.indexOf(window.uuid) !== -1;
+    isHidden(windowUUID: string): boolean {
+        return hiddenWindowIDs.indexOf(windowUUID) !== -1;
     },
-    isShown(window: Window): boolean {
-        return !this.isHidden(window);
+    isShown(windowUUID: string): boolean {
+        return !this.isHidden(windowUUID);
     },
-    toggleVisibility(window: Window): void {
-        if (this.isHidden(window)) {
-            this.show(window);
+    toggleVisibility(windowUUID: string): void {
+        if (this.isHidden(windowUUID)) {
+            this.show(windowUUID);
         } else {
-            this.hide(window);
+            this.hide(windowUUID);
         }
+    },
+    getActiveWindow(): Window {
+        return openWindows[0];
+    },
+    isActiveWindow(windowUUID: string): boolean {
+        return this.getActiveWindow().uuid === windowUUID;
+    },
+    focusWindow(windowUUID: string): void {
+        if (this.isActiveWindow(windowUUID)) return;
+
+        this.show(windowUUID);
+
+        let resolvedWindow = this.getWindow(windowUUID);
+        
+        if (!resolvedWindow) return;
+
+        // move window in openWindows to index 0
+        openWindows = openWindows.filter((w) => w.uuid !== resolvedWindow?.uuid);
+        openWindows.unshift(resolvedWindow);
+        
     }
 }
 
